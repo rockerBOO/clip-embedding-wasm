fe955ab3dacd1dfbeab87e22e5c5b85ead0deb70
rockerBOO <rockerboo@gmail.com>
2024-05-24 15:54:15 -0400

    Get embedding, show image, download embedding

--- main.js
+++ main.js
@@ -1,7 +1,47 @@
-// We only need `startup` here which is the main entry point
-// In theory, we could also use all other functions/struct types from Rust which we have bound with
-// `#[wasm_bindgen]`
-const { startup } = wasm_bindgen;
+"use strict";
+const h = React.createElement;
+
+const isAdvancedUpload = (function () {
+  var div = document.createElement("div");
+  return (
+    ("draggable" in div || ("ondragstart" in div && "ondrop" in div)) &&
+    "FormData" in window &&
+    "FileReader" in window
+  );
+})();
+
+if (isAdvancedUpload) {
+  document.querySelector("#dropbox").classList.add("has-advanced-upload");
+}
+
+const dropbox = document.querySelector("#dropbox");
+
+[
+  "drag",
+  "dragstart",
+  "dragend",
+  "dragover",
+  "dragenter",
+  "dragleave",
+  // "drop",
+].forEach((evtName) =>
+  dropbox.addEventListener(evtName, (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+  }),
+);
+
+["dragover", "dragenter"].forEach((evtName) => {
+  dropbox.addEventListener(evtName, () => {
+    dropbox.classList.add("is-dragover");
+  });
+});
+
+["dragleave", "dragend", "drop"].forEach((evtName) => {
+  dropbox.addEventListener(evtName, () => {
+    dropbox.classList.remove("is-dragover");
+  });
+});
 
 async function run_wasm() {
   // Load the wasm file by awaiting the Promise returned by `wasm_bindgen`
@@ -16,10 +56,149 @@ async function run_wasm() {
 
   const worker = new Worker("./worker.js");
 
-  setTimeout(() => {
-    console.log("Hello world post");
-    worker.postMessage({ hello: "world" });
-  }, 100);
+  worker.addEventListener("message", (msg) => {
+    switch (msg.data.t) {
+      case "loadingModel":
+        console.log("loading model...");
+        console.time("loadingModel");
+        break;
+      case "modelLoaded":
+        console.timeEnd("loadingModel");
+        dropbox.querySelector("#box_upload").classList.remove("hidden");
+        dropbox.querySelector("#box_upload").classList.add("visible");
+        document.querySelector("#file").disabled = false;
+        dropbox.querySelector("#model_loading").classList.add("hidden");
+        worker.postMessage({ t: "imageFeatures" });
+        break;
+
+      case "imageFeatures":
+        console.log("image features", msg.data.payload);
+        showEmbeddings({
+          embeddings: msg.data.payload,
+          worker,
+          file: msg.data.file,
+          imageFilename: msg.data.imageFilename,
+        });
+        break;
+
+      case "embeddingFile":
+        console.log("embeddingFile", msg.data.payload);
+        // showFile({ file: msg.data.payload });
+        break;
+    }
+  });
+
+  ["drop"].forEach((evtName) => {
+    document.addEventListener(evtName, async (e) => {
+      e.preventDefault();
+      e.stopPropagation();
+
+      const droppedFiles = e.dataTransfer.files;
+      for (let i = 0; i < droppedFiles.length; i++) {
+        processFile(droppedFiles.item(i));
+      }
+    });
+
+    document
+      .querySelector("#file")
+      .addEventListener("change", async function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+
+        const files = e.target.files;
+
+        for (let i = 0; i < files.length; i++) {
+          processFile(files.item(i), worker);
+        }
+      });
+  });
+}
+
+function Embedding({ embedding, file }) {
+  const blob = new Blob([file]);
+  const src = URL.createObjectURL(blob);
+  return h(
+    "div",
+    {},
+    h("div", {}, h("h2", {}, "CLIP image embedding")),
+    h(
+      "div",
+      { className: "embedding-results" },
+      h("img", { src, className: "embedding-image" }),
+      h("textarea", {
+        rows: 10,
+        className: "embedding",
+        value: embedding.join(","),
+      }),
+    ),
+  );
+}
+
+function MakeFile({ embeddings, worker, imageFilename }) {
+  return embeddings.map((e, i) =>
+    h(
+      "button",
+      {
+        key: i,
+        onClick: () => {
+          worker.postMessage({ t: "makeFile", payload: e, imageFilename });
+          worker.addEventListener("message", (e) => {
+            switch (e.data.t) {
+              case "embeddingFile":
+                const a = document.createElement("a");
+                a.href = e.data.payload;
+                a.download = `${e.data.imageFilename.replace(/\.[^/.]+$/, "")}.safetensors`;
+
+                var container = document.body;
+                container.appendChild(a);
+                a.click();
+
+                a.remove();
+                break;
+            }
+          });
+        },
+      },
+      "Download embedding file (safetensors)",
+    ),
+  );
+}
+
+function showEmbeddings({ embeddings, worker, file, imageFilename }) {
+  dropbox.classList.remove("box__open");
+  dropbox.classList.add("box__closed");
+  document.querySelector("#jumbo").classList.remove("jumbo__intro");
+  // document.querySelector("#note").classList.add("hidden");
+  const root = ReactDOM.createRoot(document.getElementById("results"));
+  root.render(
+    h(
+      "div",
+      {},
+      embeddings.map((embedding, i) =>
+        h(Embedding, { key: i, embedding, file }),
+      ),
+      h(MakeFile, { embeddings, worker, imageFilename }),
+    ),
+  );
+}
+
+function showFile({ file }) {
+  const root = ReactDOM.createRoot(document.getElementById("file_con"));
+  root.render(
+    h(
+      "div",
+      {},
+      h("a", { href: file, download: "embedding.safetensors" }, "Download"),
+    ),
+  );
+}
+
+function addErrorMessage(message) {
+  console.error(message);
+}
+
+async function processFile(file, worker) {
+  worker.postMessage({ t: "fileUpload", file });
 }
 
 run_wasm();
--- worker.js
+++ worker.js
@@ -5,22 +5,33 @@ importScripts("./pkg/clip_embedding_wasm.js");
 
 console.log("Initializing worker");
 
-async function fetchArrayBuffer(url) {
-  const cacheName = "clip-cache";
-  const cache = await caches.open(cacheName);
-  const cachedResponse = await cache.match(url);
-  if (cachedResponse) {
-    const data = await cachedResponse.arrayBuffer();
-    return new Uint8Array(data);
-  }
-  const res = await fetch(url, { cache: "force-cache" });
-  cache.put(url, res.clone());
-  return new Uint8Array(await res.arrayBuffer());
+// async function fetchArrayBuffer(url) {
+//   const cacheName = "clip-cache";
+//   const cache = await caches.open(cacheName);
+//   const cachedResponse = await cache.match(url);
+//   if (cachedResponse) {
+//     const data = await cachedResponse.arrayBuffer();
+//     return new Uint8Array(data);
+//   }
+//   const res = await fetch(url, { cache: "force-cache" });
+//   cache.put(url, res.clone());
+//   return new Uint8Array(await res.arrayBuffer());
+// }
+
+async function readFile(file) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onload = function (e) {
+      const buffer = new Uint8Array(e.target.result);
+      resolve(buffer);
+    };
+    reader.readAsArrayBuffer(file);
+  });
 }
 
 // In the worker, we have a different struct that we want to use as in
 // `index.js`.
-const { test_model } = wasm_bindgen;
+const { to_safetensors_buffer, ModelLocation, ClipModel } = wasm_bindgen;
 
 async function init_wasm_in_worker() {
   // Load the wasm file by awaiting the Promise returned by `wasm_bindgen`.
@@ -29,19 +40,49 @@ async function init_wasm_in_worker() {
   // // Create a new object of the `NumberEval` struct.
   // var num_eval = NumberEval.new();
 
-  // Set callback to handle messages passed to the worker.
+  const modelLocation = new ModelLocation(
+    "https://huggingface.co/openai/clip-vit-base-patch32/resolve/refs%2fpr%2f15",
+    "model.safetensors",
+  );
+
+  self.postMessage({ t: "loadingModel" });
+
+  const clipModel = await new ClipModel(modelLocation);
+
+  console.log("clip model", clipModel);
+
+  self.postMessage({ t: "modelLoaded" });
+
   self.onmessage = async (event) => {
-    // By using methods of a struct as reaction to messages passed to the
-    // worker, we can preserve our state between messages.
-    // var worker_result = num_eval.is_even(event.data);
-    console.time("test_model");
-    const result = await test_model();
-    console.timeEnd("test_model");
+    switch (event.data.t) {
+      case "fileUpload":
+        console.log("file", event.data.file);
+        const file = await readFile(event.data.file);
 
-    console.log(result);
+        console.time("image features");
+        const features = await clipModel.image_features_from_buffer(file);
+        console.timeEnd("image features");
+        self.postMessage({
+          t: "imageFeatures",
+          payload: features,
+          file,
+          imageFilename: event.data.file.name,
+        });
+        break;
 
-    // Send response back to be handled by callback in main thread.
-    self.postMessage(result);
+      case "makeFile":
+        console.log("make file payload", event.data.payload);
+        const buffer = await to_safetensors_buffer(event.data.payload);
+        console.log("file buffer", buffer);
+        const blob = new Blob([buffer]);
+        const url = URL.createObjectURL(blob);
+        self.postMessage({
+          t: "embeddingFile",
+          payload: url,
+          imageFilename: event.data.imageFilename,
+        });
+        break;
+    }
   };
 }
 
